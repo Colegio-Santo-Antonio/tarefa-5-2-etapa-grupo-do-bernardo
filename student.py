f231797a67d1244527d869d96bd087a2f71c4722
csa16-6132 <csa16-6132@colegiosantoantonio.com.br>
2025-09-07 21:25:21 -0300

    Update student.py

--- student.py
+++ student.py
@@ -1,8 +1,27 @@
 # Leia uma linha com o número do cartão
 numero = input()
 
-# TODO: implemente a verificação pelo algoritmo de Luhn
-# Siga as dicas do README.
+# Remove espaços (caso existam)
+numero = numero.replace(" ", "")
 
-# Ao final, imprima exatamente:
-# print("Cartão válido")  ou  print("Cartão inválido")
+# Convertemos para uma lista de inteiros
+digitos = [int(d) for d in numero]
+
+# Começamos da direita (posição -1), então invertemos para facilitar
+digitos = digitos[::-1]
+
+soma = 0
+
+for i, d in enumerate(digitos):
+    if i % 2 == 1:  # cada segundo dígito (começando do penúltimo)
+        d = d * 2
+        if d > 9:
+            d -= 9
+    soma += d
+
+# Verificamos se a soma é divisível por 10
+if soma % 10 == 0:
+    print("Cartão válido")
+else:
+    print("Cartão inválido")
+  
